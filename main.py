32dfa2fa9609db5838aa4e9c31fbdfee1c17799b
Tehreem Fatima <tehreemfatima3340@gmail.com>
2025-08-01 02:03:08 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from RPS_game import play, quincy, abbey, kris, mr_x
+from RPS import player
+
+print("Quincy:", play(player, quincy, 1000))
+print("Abbey:", play(player, abbey, 1000))
+print("Kris:", play(player, kris, 1000))
+print("Mr. X:", play(player, mr_x, 1000))
